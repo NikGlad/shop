a64298425e8af15e6c670ecb97e7554630995182
NikGlad <90272508+NikGlad@users.noreply.github.com>
2022-11-09 10:46:01 +0300

    09.11

--- myshop/models.py
+++ myshop/models.py
@@ -1,20 +1,25 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=100, unique=True)
 
-
     class Meta:
-        ordering = ('name', )
+        ordering = ('name',)
+        verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('myshop:product_list_by_category', args=[self.slug])
+
 class Product(models.Model):
     category = models.ForeignKey(Category,
-                                 related_name='prducts',
+                                 related_name='products',
                                  on_delete=models.CASCADE)
 
     name = models.CharField(max_length=150, db_index=True)
@@ -27,10 +32,13 @@ class Product(models.Model):
     uploaded = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ('name', )
+        ordering = ('name',)
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
         index_together = (('id', 'slug'), )
 
     def __str__(self):
-        return self.name
+        return  self.name
+
+    def get_absolute_url(self):
+        return reverse('myshop:product_detail', args=[self.id, self.slug])
